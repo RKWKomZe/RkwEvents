0713844ca8dc931233eaf77555046e1b20f077bf
Maximilian Faessler <maximilian@faesslerweb.de>
2019-02-06 14:23:00 +0000

    [Bugfix] Adjusting BeUserModel for RkwRegistration dependency management

--- Classes/Domain/Model/BackendUser.php
+++ Classes/Domain/Model/BackendUser.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace RKW\RkwEvents\Domain\Model;
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
+    /*
+     * This file is part of the TYPO3 CMS project.
+     *
+     * It is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License, either version 2
+     * of the License, or any later version.
+     *
+     * For the full copyright and license information, please read the
+     * LICENSE.txt file that was distributed with this source code.
+     *
+     * The TYPO3 project - inspiring people to share!
+     */
 
 /**
  * Class BackendUser
@@ -24,8 +24,14 @@ namespace RKW\RkwEvents\Domain\Model;
  * @package RKW_RkwEvents
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
-class BackendUser extends \RKW\RkwRegistration\Domain\Model\BackendUser
-{
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('rkw_registration')) {
+    class BackendUser extends \RKW\RkwRegistration\Domain\Model\BackendUser
+    {
 
+    }
+} else {
+    class BackendUser extends \TYPO3\CMS\Extbase\Domain\Model\BackendUser
+    {
 
+    }
 }
