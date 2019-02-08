c92689151365db3a215beebdfd480d6d9e11912f
Maximilian Faessler <maximilian@faesslerweb.de>
2019-02-08 10:34:32 +0000

    [EDIT] PHPdocs change

--- Classes/Domain/Model/BackendUser.php
+++ Classes/Domain/Model/BackendUser.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace RKW\RkwEvents\Domain\Model;
-    /*
-     * This file is part of the TYPO3 CMS project.
-     *
-     * It is free software; you can redistribute it and/or modify it under
-     * the terms of the GNU General Public License, either version 2
-     * of the License, or any later version.
-     *
-     * For the full copyright and license information, please read the
-     * LICENSE.txt file that was distributed with this source code.
-     *
-     * The TYPO3 project - inspiring people to share!
-     */
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
 /**
  * Class BackendUser
@@ -22,15 +22,6 @@ namespace RKW\RkwEvents\Domain\Model;
  * @author Steffen Kroggel <developer@steffenkroggel.de>
  * @copyright Rkw Kompetenzzentrum
  * @package RKW_RkwEvents
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
- */
-/**
- * Class BackendUser
- *
- * @author Maximilian Fäßler <maximilian@faesslerweb.de>
- * @author Steffen Kroggel <developer@steffenkroggel.de>
- * @copyright Rkw Kompetenzzentrum
- * @package RKW_RkwRegistration
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
 class BackendUser extends \TYPO3\CMS\Extbase\Domain\Model\BackendUser
